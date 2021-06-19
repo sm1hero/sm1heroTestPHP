0ee9f67fde9ea7474d280109a5ffd73303a832b8
Andrei <56765160+sm1hero@users.noreply.github.com>
2021-06-20 01:05:14 +0300

    Delete MySQL directory

--- MySQL/mydb_authorbook.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `authorbook`;
-CREATE TABLE `authorbook` (
-  `author_id` int NOT NULL,
-  `book_id` int NOT NULL,
-  PRIMARY KEY (`author_id`,`book_id`),
-  KEY `fk_book_id` (`book_id`),
-  CONSTRAINT `fk_author_id` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`),
-  CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- MySQL/mydb_book.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `book`;
-CREATE TABLE `book` (
-  `book_id` int NOT NULL,
-  `book_name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
