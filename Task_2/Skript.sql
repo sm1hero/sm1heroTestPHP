598986d7596bcb798047f43686798de218c45990
Andrei <56765160+sm1hero@users.noreply.github.com>
2021-06-20 01:06:44 +0300

    Add files via upload

---
+++ Task_2/Skript.sql
@@ -0,0 +1,10 @@
+SELECT 
+    a.author_name, a.author_surname, author_otch, COUNT(*) books
+FROM
+    author a,
+    authorbook ab
+WHERE
+    ab.author_id = a.author_id
+GROUP BY 
+	a.author_name
+HAVING COUNT(*) <= 10
