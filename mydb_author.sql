e45e2211506b8d4e015d03a1785cb80e3604fedf
Andrei <56765160+sm1hero@users.noreply.github.com>
2021-06-19 16:29:54 +0300

    Delete mydb_author.sql

--- mydb_author.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `author`;
-CREATE TABLE `author` (
-  `author_id` int NOT NULL,
-  `author_name` varchar(45) NOT NULL,
-  `author_surname` varchar(45) NOT NULL,
-  `author_otch` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
